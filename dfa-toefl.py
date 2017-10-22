cfb79c531abf3c2fd9982bed59473c29a933b473
Samson-Dai <dscsamson@gmail.com>
2017-10-21 22:49:29 -0400

    Finish dfa

--- dfa-toefl.py
+++ dfa-toefl.py
@@ -11,12 +11,26 @@ def read_rules(line, rule_num):
     new_state = line_list[2]
     dfa_machine[init_state][input_symbol]= [new_state,rule_num]
 
+def do_test(line):
+    global dfa_machine, current_state, accepting_states
+    for i in range(0, len(line)):
+        letter = line[i]
+        init_state = current_state
+        new_state = dfa_machine[current_state][letter][0]
+        rule_num = dfa_machine[current_state][letter][1]
+        print(str(i+1)+","+str(rule_num)+","+str(init_state)+","+str(letter)+","+str(new_state))
+        current_state = new_state
+    if current_state in accepting_states:
+        print("Accepted")
+    else:
+        print("Rejected")
 
 machine_name = ""
 alphabet = []
 states = []
 start_state = ""
 accepting_states = []
+current_state =""
 dfa_machine = {}
 
 
@@ -42,24 +56,27 @@ try:
             print("Alphabet: " + str(alphabet))
         elif i ==2:
             states = line.split(',')
+            for state in states:
+                dfa_machine[state] = {}
             print("States: " + str(states))
         elif i == 3:
             start_state = line
+            current_state = start_state
             print("Start state : " + start_state)
         elif i ==4:
             accepting_states = line.split(',')
             print("Accepting state : " + str(accepting_states))
         else:
             print("Rule "+ str(i-4) +" : "+ line)
-
             read_rules(line, i-4)
 
-    print(dfa_machine)
-
     for line in test:
-        print line
+        line = line.rstrip()
+        print("String : " + line)
+        do_test(line)
 
 except:
     print "Cannot read the file"
 finally:
     fa.close()
+    test.close()
